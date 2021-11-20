203581702b1a7d17c1251229413789cc93e9c5be
Valerii Diachenko <leriy.dyachenko@gmail.com>
2021-11-20 11:15:07 +0200

    Initial commit

---
+++ Source/RoguelikeCourse/RoguelikeCourse.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RoguelikeCourse.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RoguelikeCourse, "RoguelikeCourse" );
