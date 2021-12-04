4b52200abec66ce767d821ce7e3a14620a2a6490
Valerii Diachenko <leriy.dyachenko@gmail.com>
2021-12-04 08:54:45 +0200

    Create gameplay interface

---
+++ Source/RoguelikeCourse/Private/VDGameplayInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VDGameplayInterface.h"
+
+// Add default functionality here for any IVDGameplayInterface functions that are not pure virtual.
