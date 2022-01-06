d676448ffeebcddcfed7217e8e6c5b82d4fca2c0
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-01-06 21:40:15 +0200

    Fix collision with self in projectile

--- Source/RoguelikeCourse/Private/VDMagicProjectile.cpp
+++ Source/RoguelikeCourse/Private/VDMagicProjectile.cpp
@@ -33,7 +33,7 @@ AVDMagicProjectile::AVDMagicProjectile()
 void AVDMagicProjectile::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if(OtherActor)
+	if(OtherActor && OtherActor != GetInstigator())
 	{
 		UVDAttributeComponent* AttributeComponent = Cast<UVDAttributeComponent>(OtherActor->GetComponentByClass(UVDAttributeComponent::StaticClass()));
 		if(AttributeComponent)
