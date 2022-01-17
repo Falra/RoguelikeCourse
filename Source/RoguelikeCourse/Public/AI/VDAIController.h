053b486fb692b464dce53786eb00fc98f05e5748
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-01-17 19:14:47 +0200

    Add blank classes for AI and controller

---
+++ Source/RoguelikeCourse/Public/AI/VDAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "VDAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROGUELIKECOURSE_API AVDAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
