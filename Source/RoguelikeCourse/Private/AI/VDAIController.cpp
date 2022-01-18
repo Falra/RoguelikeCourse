2725fe2229c3ff6dcd47c93a3ca0ebe0c76a88a6
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-01-18 09:12:02 +0200

    Set up first location to move for AI

--- Source/RoguelikeCourse/Private/AI/VDAIController.cpp
+++ Source/RoguelikeCourse/Private/AI/VDAIController.cpp
@@ -3,9 +3,18 @@
 
 #include "AI/VDAIController.h"
 
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
+
 void AVDAIController::BeginPlay()
 {
 	Super::BeginPlay();
 
 	RunBehaviorTree(BehaviorTree);
+
+	APawn* MyPawn = UGameplayStatics::GetPlayerPawn(this, 0);
+	if(MyPawn)
+	{
+		GetBlackboardComponent()->SetValueAsVector("MoveToLocation", MyPawn->GetActorLocation());
+	}
 }
