11f946c4e1459d329e0aae073b1abda0b3b7216b
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-01-19 09:07:56 +0200

    Add AIModule in build

--- Source/RoguelikeCourse/RoguelikeCourse.Build.cs
+++ Source/RoguelikeCourse/RoguelikeCourse.Build.cs
@@ -8,7 +8,7 @@ public class RoguelikeCourse : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTasks" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "GameplayTasks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
