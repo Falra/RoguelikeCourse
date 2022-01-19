e8bebc3f96b3c37b77633c55954ba471629debcf
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-01-19 09:24:01 +0200

    Add WithinAttackRange check node implementation

--- Source/RoguelikeCourse/Private/AI/VDBTService_CheckAttackRange.cpp
+++ Source/RoguelikeCourse/Private/AI/VDBTService_CheckAttackRange.cpp
@@ -3,7 +3,31 @@
 
 #include "AI/VDBTService_CheckAttackRange.h"
 
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
 void UVDBTService_CheckAttackRange::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
 	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	// check distance to player
+	UBlackboardComponent* BlackboardComponent = OwnerComp.GetBlackboardComponent();
+	if(ensure(BlackboardComponent))
+	{
+		AActor* PlayerActor = Cast<AActor>(BlackboardComponent->GetValueAsObject("TargetActor"));
+		if(PlayerActor)
+		{
+			AAIController* AIController = OwnerComp.GetAIOwner();
+			if(ensure(AIController))
+			{
+				APawn* AIPawn = AIController->GetPawn();
+				if(ensure(AIPawn))
+				{
+					float DistanceToPlayer = FVector::Distance(PlayerActor->GetActorLocation(), AIPawn->GetActorLocation());
+					bool bWithinAttackRange = DistanceToPlayer < 2000.0f;
+					BlackboardComponent->SetValueAsBool(AttackRangeKey.SelectedKeyName, bWithinAttackRange);
+				}
+			}
+		}
+	}
 }
--- Source/RoguelikeCourse/Public/AI/VDBTService_CheckAttackRange.h
+++ Source/RoguelikeCourse/Public/AI/VDBTService_CheckAttackRange.h
@@ -15,6 +15,10 @@ class ROGUELIKECOURSE_API UVDBTService_CheckAttackRange : public UBTService
 	GENERATED_BODY()
 
 protected:
+
+	UPROPERTY(EditAnywhere, Category = "AI")
+	FBlackboardKeySelector AttackRangeKey;
+	
 	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
 	
 };
