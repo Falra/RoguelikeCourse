65e8db7d025eb774da493105978d7759eddce02f
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-01-20 08:36:28 +0200

    Add blank class for attack node

---
+++ Source/RoguelikeCourse/Public/AI/VDBTTask_RangedAttack.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "VDBTTask_RangedAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROGUELIKECOURSE_API UVDBTTask_RangedAttack : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
