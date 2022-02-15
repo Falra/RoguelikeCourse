cac7a761026b27a249f23b3ec8bd2e43e54cfe9c
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-02-15 19:19:39 +0200

    #15.3 Add apply damage and directional damage functions

---
+++ Source/RoguelikeCourse/Private/VDGameplayFunctionLibrary.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VDGameplayFunctionLibrary.h"
+
+#include "VDAttributeComponent.h"
+
+bool UVDGameplayFunctionLibrary::ApplyDamage(AActor* DamageCauser, AActor* TargetActor, float DamageAmount)
+{
+	UVDAttributeComponent* AttributeComp = UVDAttributeComponent::GetAttributes(TargetActor);
+	if (AttributeComp)
+	{
+		return AttributeComp->ApplyHealthChange(DamageCauser, -DamageAmount);
+	}
+	return false;
+}
+
+bool UVDGameplayFunctionLibrary::ApplyDirectionalDamage(AActor* DamageCauser, AActor* TargetActor, float DamageAmount,
+	const FHitResult& HitResult)
+{
+	if(ApplyDamage(DamageCauser, TargetActor, DamageAmount))
+	{
+		UPrimitiveComponent* HitComp = HitResult.GetComponent();
+		if(HitComp && HitComp->IsSimulatingPhysics(HitResult.BoneName))
+		{
+			HitComp->AddImpulseAtLocation(-HitResult.ImpactNormal * 300000.f, HitResult.ImpactPoint, HitResult.BoneName);
+		}
+		return true;
+	}
+	return false;
+}
