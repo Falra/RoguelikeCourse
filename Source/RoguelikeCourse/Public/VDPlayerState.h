3e2d62b610fe80b6ca39675402f8138ebfd31f74
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-03-07 13:11:37 +0200

    #18.1 Add player state for credits

---
+++ Source/RoguelikeCourse/Public/VDPlayerState.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "VDPlayerState.generated.h"
+
+class AVDPlayerState;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnCreditsChanged, AVDPlayerState*, PlayerState, int32, NewCredits, int32, Delta);
+
+/**
+ * 
+ */
+UCLASS()
+class ROGUELIKECOURSE_API AVDPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+protected:
+
+	int32 Credits;
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "PlayerState|Credits") // < Category|SubCategory
+	void AddCredits(int32 Delta);
+
+	UFUNCTION(BlueprintCallable, Category = "PlayerState|Credits")
+	bool RemoveCredits(int32 Delta);
+
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnCreditsChanged OnCreditsChanged;	
+};
