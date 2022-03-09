858a88c35e29585a2715fb4b13c91057e0c17a27
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-03-09 18:01:24 +0200

    #18.2 Add blank class for action effect

---
+++ Source/RoguelikeCourse/Public/VDActionEffect.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VDAction.h"
+#include "VDActionEffect.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROGUELIKECOURSE_API UVDActionEffect : public UVDAction
+{
+	GENERATED_BODY()
+	
+};
