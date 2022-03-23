08e4c90d493ce354719d95768244e5c5795e7a7e
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-03-23 14:10:19 +0200

    #20.4 Check for AI work through net

--- Source/RoguelikeCourse/Private/VDProjectileBase.cpp
+++ Source/RoguelikeCourse/Private/VDProjectileBase.cpp
@@ -33,7 +33,7 @@ AVDProjectileBase::AVDProjectileBase()
 	ImpactShakeInnerRadius = 250.0f;
 	ImpactShakeOuterRadius = 2500.0f;
 
-	//SetReplicates(true);
+	SetReplicates(true);
 }
 
 void AVDProjectileBase::OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
