3d8bff72c4b6aba9c2ae8f488339f563631d400b
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-04-01 15:14:48 +0300

    #24.4 Add pause menu

--- Source/RoguelikeCourse/Private/VDPlayerController.cpp
+++ Source/RoguelikeCourse/Private/VDPlayerController.cpp
@@ -3,6 +3,8 @@
 
 #include "VDPlayerController.h"
 
+#include "Blueprint/UserWidget.h"
+
 void AVDPlayerController::SetPawn(APawn* InPawn)
 {
 	Super::SetPawn(InPawn);
@@ -15,10 +17,38 @@ void AVDPlayerController::BeginPlayingState()
 	BlueprintBeginPlayingState();
 }
 
-
 void AVDPlayerController::OnRep_PlayerState()
 {
 	Super::OnRep_PlayerState();
 
 	OnPlayerStateReceived.Broadcast(PlayerState);
+}
+
+void AVDPlayerController::TogglePauseMenu()
+{
+	if(PauseMenuInstance && PauseMenuInstance->IsInViewport())
+	{
+		PauseMenuInstance->RemoveFromParent();
+		PauseMenuInstance = nullptr;
+
+		bShowMouseCursor = false;
+		SetInputMode(FInputModeGameOnly());
+
+		return;
+	}
+	
+	PauseMenuInstance = CreateWidget<UUserWidget>(this, PauseMenuClass);
+	if(PauseMenuInstance)
+	{
+		PauseMenuInstance->AddToViewport(100);
+		bShowMouseCursor = true;
+		SetInputMode(FInputModeUIOnly());
+	}
+}
+
+void AVDPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	InputComponent->BindAction("PauseMenu", IE_Pressed, this, &AVDPlayerController::TogglePauseMenu);
 }
--- Source/RoguelikeCourse/Public/VDPlayerController.h
+++ Source/RoguelikeCourse/Public/VDPlayerController.h
@@ -20,6 +20,17 @@ class ROGUELIKECOURSE_API AVDPlayerController : public APlayerController
 
 protected:
 
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<UUserWidget> PauseMenuClass;
+
+	UPROPERTY()
+	UUserWidget* PauseMenuInstance;
+
+	UFUNCTION(BlueprintCallable)
+	void TogglePauseMenu();
+
+	virtual void SetupInputComponent() override;
+
 	UPROPERTY(BlueprintAssignable)
 	FOnPawnChanged OnPawnChanged;
 
