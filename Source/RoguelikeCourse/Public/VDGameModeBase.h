2aa75bee30030eef61cf9906fe8ea0d173767190
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-04-06 21:00:35 +0300

    #27.1 Add the base for a data table with a monster data

--- Source/RoguelikeCourse/Public/VDGameModeBase.h
+++ Source/RoguelikeCourse/Public/VDGameModeBase.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/DataTable.h"
 #include "GameFramework/GameModeBase.h"
 #include "EnvironmentQuery/EnvQueryTypes.h"
 #include "VDGameModeBase.generated.h"
@@ -11,6 +12,39 @@ class UEnvQuery;
 class UEnvQueryInstanceBlueprintWrapper;
 class UCurveFloat;
 class UVDSaveGame;
+class UDataTable;
+
+/* DataTable Row for spawning monsters in game mode */
+USTRUCT(BlueprintType)
+struct FMonsterInfoRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+
+	FMonsterInfoRow()
+	{
+		Weight = 1.0f;
+		SpawnCost = 5.0f;
+		KillReward = 20.0f;
+	}
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSubclassOf<AActor> MonsterClass;
+
+	/* Relative chance to pick this monster */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)	
+	float Weight;
+
+	/* Points required by game mode to spawn this unit */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float SpawnCost;
+
+	/* Amount of credits awarded to kill of this unit */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float KillReward;
+};
+
 
 /**
  * 
@@ -58,7 +92,11 @@ protected:
 	FString SlotName;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "SaveGame")
-	UVDSaveGame* CurrentSaveGame; 
+	UVDSaveGame* CurrentSaveGame;
+
+	/* All monster table */
+	UPROPERTY(EditDefaultsOnly, Category = "AI")
+	UDataTable* MonsterTable;
 
 	UFUNCTION()
 	void OnSpawnBotQueryCompleted(UEnvQueryInstanceBlueprintWrapper* QueryInstance, EEnvQueryStatus::Type QueryStatus);
